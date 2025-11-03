7e3431e17d68a12506efd7e038b907c18c381f2c
danielSenne <danielflausinosenne123@gmail.com>
2025-11-03 12:57:17 -0300

    Estilização do projeto de troca de lado dos dados de forma aleatória

---
+++ app__dados/android/app/src/main/kotlin/com/example/app__dados/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app__dados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app__dados/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade700,
+        appBar: AppBar(
+          title: Text('Dadoos'),
+          backgroundColor: Colors.teal.shade900,
+        ),
+        body: Dadoos(),
+      ),
+    ),
+  );
+}
+
+class Dadoos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(children:[
+      Expanded(
+        child: 
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset('imagens/dado1.png'),
+        )
+       ),
+      Expanded(
+        child: 
+         Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: Image.asset('imagens/dado2.png'),
+         )
+       )
+       ],
+      ),
+    );
+  }
+}
