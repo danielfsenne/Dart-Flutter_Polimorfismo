e641f820da73b316c68d6c82e7207fc29fbe3394
danielSenne <danielflausinosenne123@gmail.com>
2025-11-09 12:14:42 -0300

    Conclusão do projeto e novos aprendizados commo arrow function, entrada e saída de função

--- app_marimba/lib/main.dart
+++ app_marimba/lib/main.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(MarimbaApp());
+void main() => runApp(const MarimbaApp());
 
 class MarimbaApp extends StatelessWidget {
-  final player = AudioPlayer(); 
+  const MarimbaApp({super.key});
+
+  Future<void> tocarSom(int numeroDoSom) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('nota$numeroDoSom.wav'));
+  }
+
+  Expanded criarBotao({required Color cor, required int numeroDaNota}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () => tocarSom(numeroDaNota),
+        style: TextButton.styleFrom(backgroundColor: cor),
+        child: const SizedBox.shrink(), 
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                await player.play(AssetSource('nota1.wav'));
-              },
-              child: const Text('Clique em mim'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              criarBotao(cor: Colors.amber, numeroDaNota: 1),
+              criarBotao(cor: Colors.orange, numeroDaNota: 2),
+              criarBotao(cor: Colors.red, numeroDaNota: 3),
+              criarBotao(cor: Colors.lime, numeroDaNota: 4),
+              criarBotao(cor: Colors.blue, numeroDaNota: 5),
+              criarBotao(cor: Colors.lightGreen, numeroDaNota: 6),
+              criarBotao(cor: Colors.purple, numeroDaNota: 7),
+            ],
           ),
         ),
       ),
